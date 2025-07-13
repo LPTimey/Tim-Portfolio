31f13709abcd600f6816334416c26948223b3d95
LPTimey <86461861+LPTimey@users.noreply.github.com>
2025-07-13 14:29:05 +0200

    Update three_js_setup.rs

--- src/components/three_js_setup.rs
+++ src/components/three_js_setup.rs
@@ -7,8 +7,8 @@ pub fn import_map(page:Page)->Markup{
         script type="importmap"{(PreEscaped(format!(r#"
         {{
             "imports": {{
-                "three": "{path}vendor/three.js/three.js r176/build/three.module.js",
-                "three/addons/": "{path}vendor/three.js/three.js r176/examples/jsm/"
+                "three": "{path}vendor/three.js/three.js-r178/build/three.module.js",
+                "three/addons/": "{path}vendor/three.js/three.js-r178/examples/jsm/"
             }}
         }}
         "#,path=page.path_to_root())))}
