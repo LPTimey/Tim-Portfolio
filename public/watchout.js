49748e7cef42bbafc5b2b0f57a243718268cfd05
LPTimey <86461861+LPTimey@users.noreply.github.com>
2026-01-18 14:44:04 +0100

    Update watchout.js

--- public/watchout.js
+++ public/watchout.js
@@ -13,15 +13,8 @@ const infoCanvas = /** */(document.getElementById("WatchInfoCanvas"));
 // @ts-ignore
 const watchPath = window.pathToRoot + `./assets/Design%20der%20Mensch%20Maschine%20Schnittstelle/WatchOut/TimUhr.glb`;
 const watch = await loader.loadAsync(watchPath);
-/** @type {THREE.Object3D} */
-// @ts-ignore
-const watchScene = watch.scene.clone();
 
 async function main() {
-    // startButton.innerHTML = play_icon;
-    // startButton.setAttribute("data-state", "play");
-    // startButton.addEventListener("click", initHero);
-    // startButton.style.visibility = "hidden";
     initInfoSpin();
 }
 
@@ -62,19 +55,13 @@ export async function initInfoSpin() {
     controls.update();
 
     function animate() {
-
-        requestAnimationFrame(animate);
-
         if (rendererNeedsResize(renderer)) {
             resize(renderer, camera);
         }
-
-        // required if controls.enableDamping or controls.autoRotate are set to true
         controls.update();
 
         renderer.render(scene, camera);
-
     }
     renderer.render(scene, camera);
-    animate()
+    renderer.setAnimationLoop(animate);
 }
